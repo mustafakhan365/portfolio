3c276060cab954cf81a202b163cbee45c9b036fb
Mustafa Khan <mustafakhan365@gmail.com>
2024-03-17 20:16:03 +0500

    Email

--- myscript.js
+++ myscript.js
@@ -6,20 +6,29 @@ jQuery(document).ready(function() {
     jQuery('.nav-item').mouseout(function(){
         jQuery('.nav-item').css("opacity","1");
     });
-    jQuery('.closemenu').on('click touchstart', function() {
-        jQuery('.navbar-collapse').removeClass('show');
-        jQuery('.navbar-toggler').addClass('collapsed');
-        jQuery('.navbar-toggler').attr('aria-expanded', 'false');
-    });
+    jQuery('.closemenu').on('click touchstart', () => closeMobileMenu());
     checkScroll();
-    $(window).scroll(function() {
-        checkScroll();
-    });
+    jQuery(window).scroll(() => checkScroll());
+    $(".viewmore").on('click touchstart', () => scrollToSection('.aboutme'));
+    jQuery(".backtotop").on('click touchstart', () => scrollToSection('.myheader'));
 });
 function checkScroll() {
-    if ($(this).scrollTop() > 150) {
-        $("#menuheader").addClass("stickyheader");
+    if (jQuery(this).scrollTop() > 200) {
+        jQuery("#menuheader").addClass("stickyheader");
+        jQuery(".backtotop").show();
     } else {
-        $("#menuheader").removeClass("stickyheader");
+        jQuery("#menuheader").removeClass("stickyheader");
+        jQuery(".backtotop").hide();
     }
+}
+function scrollToSection(section) {
+    jQuery('html, body').animate({
+        scrollTop: section === '.mycontact' ? jQuery(section).offset().top - 50 : jQuery(section).offset().top
+    }, 500);
+    closeMobileMenu();
+}
+function closeMobileMenu() {
+    jQuery('.navbar-collapse').removeClass('show');
+    jQuery('.navbar-toggler').addClass('collapsed');
+    jQuery('.navbar-toggler').attr('aria-expanded', 'false');
 }
