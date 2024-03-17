d3d51a17bdfb7b562c4c7d347bc1ea95e63c952f
Mustafa Khan <mustafakhan365@gmail.com>
2024-03-17 20:37:09 +0500

    Email Fix

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = "mustafakhan365@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Portfolio Contact | $mysubject";
-$body = "You have received a new message from your portfolio contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$body = "\n\nName: $name\n\nEmail: $emailn\\nPhone: $phone\\nSubject: $subject\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 mail($to,$subject,$body,"From:$header");
