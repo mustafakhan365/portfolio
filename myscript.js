2508a8540526cc5a2802e615a8f0536e37bf17bd
Mustafa Khan <mustafakhan365@gmail.com>
2024-03-31 21:39:01 +0500

    ScrollSpy Add

--- myscript.js
+++ myscript.js
@@ -1,25 +1,36 @@
 jQuery(document).ready(function() {
-    jQuery('.nav-item').mouseover(function(){
-        jQuery('.nav-item').css("opacity","0.5");
-        jQuery(this).css("opacity","1");
-    });
-    jQuery('.nav-item').mouseout(function(){
-        jQuery('.nav-item').css("opacity","1");
+    jQuery('.myheader .nav-item').click(function(){
+        jQuery('.myheader .nav-item').removeClass("active");
+        jQuery(this).addClass("active");
     });
     jQuery('.closemenu').on('click touchstart', () => closeMobileMenu());
     checkScroll();
     jQuery(window).scroll(() => checkScroll());
-    $(".viewmore").on('click touchstart', () => scrollToSection('.aboutme'));
+    jQuery(".viewmore").on('click touchstart', () => scrollToSection('.aboutme'));
     jQuery(".backtotop").on('click touchstart', () => scrollToSection('.myheader'));
 });
 function checkScroll() {
-    if (jQuery(this).scrollTop() > 200) {
+    let scrollposition = jQuery(this).scrollTop();
+    if (scrollposition > 200) {
         jQuery("#menuheader").addClass("stickyheader");
         jQuery(".backtotop").show();
     } else {
         jQuery("#menuheader").removeClass("stickyheader");
         jQuery(".backtotop").hide();
     }
+    if(scrollposition < (jQuery(".aboutme").offset().top - 150)) {
+        highlightSection(0);
+    } else if (scrollposition < (jQuery(".myprojects").offset().top - 150)) {
+        highlightSection(1);
+    } else if (scrollposition < (jQuery(".myexperience").offset().top - 150)) {
+        highlightSection(2);
+    } else if (scrollposition < (jQuery(".mycontact").offset().top - 150)) {
+        highlightSection(3);
+    } else { highlightSection(4); }
+}
+function highlightSection(index) {
+    jQuery('.myheader .nav-item').removeClass("active");
+    jQuery('.myheader .nav-item').eq(index).addClass("active");
 }
 function scrollToSection(section) {
     jQuery('html, body').animate({
