787fa7c8749d21bbeff4e062ce4abfb8c4167397
Mustafa Khan <mustafakhan365@gmail.com>
2024-03-31 19:15:41 +0500

    Projects View

--- projects/all/index.js
+++ projects/all/index.js
@@ -9,7 +9,7 @@ function fetchAllProjects(endpoint, containerId) {
         jQuery.each(data, function(index, project) {
           let card = `
             <div class="card border-0 bg-dark" data-bs-toggle="modal" data-bs-target="#projectsModal">
-              <img src="projects/images/${project.image}" class="card-img-top" alt="...">
+              <img src="${project.image}" class="card-img-top" alt="...">
               <div class="card-body text-white p-4">
                 <h5 class="card-title fw-bold fs-6 mb-2">${project.title}</h5>
                 <p class="card-text fw-medium mb-0">${project.category}</p>
